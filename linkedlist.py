31c2215d11f11110355092e0f9673e9dfb5490fd
erabhisek <abhishek.er45@gmail.com>
2024-08-28 20:10:44 +0530

    Make linkedlist initially empty

--- linkedlist.py
+++ linkedlist.py
@@ -3,4 +3,11 @@ class Node:
     def __init__(self, value):
         self.data = value
         self.next = None
+    
+
+class LinkedList:
+    
+    def __inti__(self):
+        self.head = None
+
 
